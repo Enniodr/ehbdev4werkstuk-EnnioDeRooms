588a2510b0095a6537ec1cea4ace94a125d0568e
Ennio De Rooms <Ennio.de.rooms@student.ehb.be>
2020-08-25 14:29:35 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,38 @@
+"use strict"
+
+$(function () {
+
+	$.ajax({
+		url: "entries.json",
+		method: "GET",
+		dataType: "json",
+	}).done(function (data) {
+		//alle items zitten in data
+		console.log(data);
+
+		//de array van objecten
+		console.log(data.items);
+
+		//categorie van 1 objet
+		console.log(data.items[0].category)
+
+		loadHTML(data);
+	});
+});
+
+function loadHTML(data) {
+
+	$.each(data, function (index, data) {
+		$("body").append(`
+	<div class="card">
+	<div class="image-wrapper">
+		<img class="foto" src="${data.thumbnail_url}">
+	</div>
+	<div class="info">
+		<h3>${data.title}</h3>
+		<p>${data.excerpt}</p>
+
+	</div>
+</div>`)
+	});
+}
