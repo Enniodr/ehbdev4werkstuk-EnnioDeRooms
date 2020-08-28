860318a08a506b25066d7a44a7538e38f99adc95
Ennio De Rooms <Ennio.de.rooms@student.ehb.be>
2020-08-28 14:33:11 +0200

    Fout (doelgroep ipv goelgroep) + Filters toegevoeg

--- script.js
+++ script.js
@@ -4,8 +4,8 @@ $(function () {
 
 	let filters = {
 		genre: [""],
-		goelgroep: [""]
-	}
+		doelgroep: [""]
+	};
 
 	$.ajax({
 		url: "entries.json",
@@ -15,9 +15,9 @@ $(function () {
 		//alle items zitten in data
 		console.log(data);
 		filteren(data.items, filters);
-		//categorie van 1 objet
+
 		//de array van objecten
-		console.log(data.items);
+		//console.log(data.items);
 		filtersOphalen(data.items, filters);
 
 	});
@@ -30,11 +30,14 @@ function loadHTML(data) {
 	$("body").append(`
 	<div class="card">
 	<div class="image-wrapper">
-		<img class="foto" src="${data.thumbnail_url}">
+		<img class="foto" src="${data.thumbnail.url}" alt="${data.slug}">
 	</div>
 	<div class="info">
-		<h3>${data.title}</h3>
-		<p>${data.excerpt}</p>
+		<h3>${data.name}</h3>
+		<p>${data["genre-v2"]}</p>
+		<p>${data.age}</p>
+		<p>${data["recorded-at"]}</p>
+		<p>${data["video-length"]}</p>
 
 	</div>
 </div>`)
@@ -47,10 +50,11 @@ function filtersOphalen(data, filters) {
 	$(".doelgroep").click(function () {
 		$(".content").text("");
 		doelgroep = geklikteFilters(this, doelgroep);
-		//console.log(doelgroep);
+		// console.log(doelgroep);
 		filters.doelgroep.splice(0, 1);
 		filters.doelgroep.push(doelgroep);
-		//console.log(filters);
+		// console.log("test");
+		// console.log(filters);
 		filteren(data, filters);
 	})
 	let genre = []
@@ -68,7 +72,7 @@ function filtersOphalen(data, filters) {
 
 function geklikteFilters(filterSoort, filters) {
 	let filter = filterSoort.value;
-	console.log(filter);
+	//console.log(filter);
 	if (filterSoort.checked) {
 		filters.push(filter);
 	} else {
@@ -80,17 +84,49 @@ function geklikteFilters(filterSoort, filters) {
 
 function filteren(data, filters) {
 
-	let doelgroep = filters.doelgroep;
-	let genre = filters.genre;
-	console.log("hieronder");
-	console.log(genre);
-	console.log(doelgroep);
+	let doelgroepFilters = filters.doelgroep;
+	let genreFilters = filters.genre;
 	//als er geen filters zijn aangeduid wordt alle data getoond.
-	if (doelgroep[0].length == 0 && genre[0].length == 0) {
+	if (doelgroepFilters[0].length == 0 && genreFilters[0].length == 0) {
 		console.log("geen");
 		console.log(data);
 		for (let dataIndex in data) {
 			loadHTML(data[dataIndex]);
+		}
+	} else {
+		//Enkel op genre
+		if (doelgroepFilters[0].length == 0) {
+			console.log("genre");
+			for (let dataIndex in data) {
+				for (let genreIndex in genreFilters[0]) {
+					if (data[dataIndex]["genre-v2"] == genreFilters[0][genreIndex]) {
+						loadHTML(data[dataIndex]);
+					}
+
+				}
+			}
+			//enkel op doelgroep
+		} else if (genreFilters[0].length == 0) {
+			console.log("doelgroep");
+			for (let dataIndex in data) {
+				for (let doelGroepIndex in doelgroepFilters[0]) {
+					if (data[dataIndex].category == doelgroepFilters[0][doelGroepIndex]) {
+						loadHTML(data[dataIndex]);
+					}
+				}
+			}
+			//op beide filteren
+		} else {
+			console.log("beide");
+			for (let dataIndex in data) {
+				for (let doelGroepIndex in doelgroepFilters[0]) {
+					for (let genreIndex in genreFilters[0]) {
+						if (data[dataIndex].category == doelgroepFilters[0][doelGroepIndex] && data[dataIndex]["genre-v2"] == genreFilters[0][genreIndex]) {
+							loadHTML(data[dataIndex]);
+						}
+					}
+				}
+			}
 		}
 	}
 }
