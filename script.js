681510fde884335e79608cd381de37a502025fe2
Ennio De Rooms <Ennio.de.rooms@student.ehb.be>
2020-08-28 13:58:48 +0200

    Data komt niet meer door

--- script.js
+++ script.js
@@ -13,23 +13,21 @@ $(function () {
 		dataType: "json",
 	}).done(function (data) {
 		//alle items zitten in data
-		//console.log(data);
-
-		//de array van objecten
-		//console.log(data.items);
-
+		console.log(data);
+		filteren(data.items, filters);
 		//categorie van 1 objet
-		console.log(data.items[0].category)
+		//de array van objecten
+		console.log(data.items);
+		filtersOphalen(data.items, filters);
 
-		loadHTML(data);
 	});
-});
+})
 
 //laad de html op de pagina nadat de filters zijn toegepast
 function loadHTML(data) {
 
-	$.each(data, function (index, data) {
-		$("body").append(`
+
+	$("body").append(`
 	<div class="card">
 	<div class="image-wrapper">
 		<img class="foto" src="${data.thumbnail_url}">
@@ -40,5 +38,59 @@ function loadHTML(data) {
 
 	</div>
 </div>`)
-	});
+
+}
+
+
+function filtersOphalen(data, filters) {
+	let doelgroep = []
+	$(".doelgroep").click(function () {
+		$(".content").text("");
+		doelgroep = geklikteFilters(this, doelgroep);
+		//console.log(doelgroep);
+		filters.doelgroep.splice(0, 1);
+		filters.doelgroep.push(doelgroep);
+		//console.log(filters);
+		filteren(data, filters);
+	})
+	let genre = []
+	$(".genre").click(function () {
+		$(".content").text("");
+		genre = geklikteFilters(this, genre);
+		filters.genre.splice(0, 1);
+		filters.genre.push(genre);
+		//console.log(filters);
+		filteren(data, filters);
+	})
+
+
+}
+
+function geklikteFilters(filterSoort, filters) {
+	let filter = filterSoort.value;
+	console.log(filter);
+	if (filterSoort.checked) {
+		filters.push(filter);
+	} else {
+		filters.splice(filters.indexOf(filter), 1);
+	}
+	return filters;
+}
+
+
+function filteren(data, filters) {
+
+	let doelgroep = filters.doelgroep;
+	let genre = filters.genre;
+	console.log("hieronder");
+	console.log(genre);
+	console.log(doelgroep);
+	//als er geen filters zijn aangeduid wordt alle data getoond.
+	if (doelgroep[0].length == 0 && genre[0].length == 0) {
+		console.log("geen");
+		console.log(data);
+		for (let dataIndex in data) {
+			loadHTML(data[dataIndex]);
+		}
+	}
 }
